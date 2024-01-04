345ceabc34940902616ed600896a97c312130ba2
RohitPatil013 <151506097+RohitPatil013@users.noreply.github.com>
2024-01-04 18:18:19 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) // Were using nodejs, not Browser
+
+console.log(3
+       +3)       // Code readability should be High don't write code like these
+
+console.log("Rohit");
+
+let name = "Rohit Patil"
+let age = 23
+let isLoggedIn = false
+
+// number ==> 2 to power of 53
+// bigint ==> any large num
+// string ==> " " // always write in quot
+// boolean ==> true / false
+// null ==> standalone value
+// undefined ==> not defined
+// symbol ==> unique / create uniqueness assigning with components in react js
+
+// object
+
+console.log(typeof 23);
+console.log(typeof "Pune");
+console.log(typeof undefined);   // undefined
+console.log(typeof null);        // object
