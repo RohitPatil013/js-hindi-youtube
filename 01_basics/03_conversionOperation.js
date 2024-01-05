8ebcd291ab0bcbad904de2fd48eb7b68b319f618
RohitPatil013 <151506097+RohitPatil013@users.noreply.github.com>
2024-01-05 10:32:16 +0000

    Conversion of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = undefined
+//let score = undefined
 
 //console.log(typeof score)
 //console.log(typeof score);
@@ -19,7 +19,50 @@ let score = undefined
 // " " ==> false  //Empty
 // "Rohit" ==> true
 
-let somenumber = 40
-let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let somenumber = 40
+//let stringNumber = String(somenumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//*****************OPERATIONS******************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2 + 2);    // Addition
+// console.log(2 - 2);    // Substraction
+// console.log(2 * 2);    // Multiplication 
+// console.log(2 ** 3);   // 2 to the power of 3
+// console.log(2 / 3);    // division
+// console.log(2 % 3);    // Reminder
+
+// let str1 = "Hello"
+// let str2 = " Rohit"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 2 / 5);
+
+// console.log(true);
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2
+
+// console.log(num2);
+
+let gameCounter = 100
+// ++gameCounter
+gameCounter++
+
+console.log(gameCounter);
+
